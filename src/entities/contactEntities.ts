153daba8ff663c80e8cb0dde122f0ca30d8761fe
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-13 13:53:10 -0300

    feat: att entities

---
+++ src/entities/contactEntities.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToMany, JoinColumn, CreateDateColumn, DeleteDateColumn} from "typeorm";
+import { User } from "./userEntities";
+
+@Entity("contacts")
+export class Contact extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({type: "varchar", length: 255, nullable: false})
+    fullname: string;
+
+    @Column({type: "varchar", length: 255, nullable: false})
+    email: string;
+
+    @Column({type: "varchar", length: 20, nullable: false})
+    phone_number: string;
+
+    @CreateDateColumn({type: "date", default: () => "CURRENT_TIMESTAMP"})
+    registration_date: string;
+
+    @DeleteDateColumn({type: "date"})
+    delete_date: string | null;
+    
+    @ManyToMany(() => User, (user) => user.contacts, {onDelete: "CASCADE"})
+    @JoinColumn({name: "user_id"})
+    user: User;
+}
+
+export default Contact;

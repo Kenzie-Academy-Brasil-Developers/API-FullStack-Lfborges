03d33774a2eb87e8f7032d09a33eaf1677e0b1f2
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-13 11:28:31 -0300

    feat: first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'express-async-errors'
+
+
+const app = express();
+app.use(express.json());
+
+app.use()
+app.use()
+app.use()
+
+app.use()
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app';
+import { AppDataSource } from 'dataSource';
+
+(async () =>{
+    try{
+        await AppDataSource.initialize();
+        console.log('Data source initiliazed sucessfully');
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Servidor sendo executado na porta ${port}`);   
+        });
+    }catch(err){
+        console.log('Error durante a inicialização do servidor: ', err);
+        
+    }
+})();

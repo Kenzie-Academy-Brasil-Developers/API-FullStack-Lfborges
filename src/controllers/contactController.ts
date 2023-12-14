3e0a8bddd51d0fbb1c3648cbed82c797c9e74907
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-14 12:59:53 -0300

    feat: new att controllers done

---
+++ src/controllers/contactController.ts
@@ -0,0 +1,29 @@
+import { Response, Request } from "express";
+import { contactService } from "../services";
+import { ContactCreate, ContactRead, ContactReturn, ContactUpdate } from "interfaces/contactInterface";
+
+const create = async (req: Request, res: Response): Promise<Response> => {
+    const payload: ContactCreate = req.body;
+    const contact: ContactReturn = await contactService.createContact(payload);
+    return res.status(201).json(contact);
+}
+
+const read = async (req: Request, res: Response): Promise<Response> => {
+    const payload: ContactRead = await contactService.readContacts();
+    return res.status(200).json(payload);
+}
+
+const update = async (req: Request, res: Response): Promise<Response> => {
+    const id: number = Number(req.params.contactId);
+    const payload: ContactUpdate = req.body;
+    const updatedContact: ContactReturn = await contactService.updateContact(id, payload);
+    return res.status(200).json(updatedContact);
+} 
+
+const destroy = async (req: Request, res: Response): Promise<Response> => {
+    const id: number = Number(req.params.contactId);
+    await contactService.destroyContact(id);
+    return res.status(204).send();
+}
+
+export {create, read, update, destroy}
---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import * as userController from './userConttroller';
+import * as sessionController from './sessionController';
+import * as contactController from './contactController';
+
+export { userController, sessionController, contactController};

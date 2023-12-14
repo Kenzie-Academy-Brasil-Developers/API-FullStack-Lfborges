a9f7a173405c43122c0796027b5bad387150c8f9
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-14 13:58:04 -0300

    feat new att for app

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import 'express-async-errors'
 import { handleError } from 'errors/handleError';
+import contactRouter from 'router/contactRoutes';
+import loginRouter from 'router/sessionRouters';
+import userRouter from 'router/userRouters';
 
 const app = express();
 app.use(express.json());
 
-app.use('/user',)
-app.use('/contacts',)
-app.use('/login', )
+app.use('/user', userRouter)
+app.use('/contacts', contactRouter)
+app.use('/login', loginRouter)
 
 app.use(handleError)
 

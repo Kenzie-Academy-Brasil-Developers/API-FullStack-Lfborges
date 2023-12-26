b3c42b3d3d1b26c9c3507307e46592fb322a1edd
Luis Eduardo Fernandes Borges <108821332+PietroNog1@users.noreply.github.com>
2023-12-26 15:07:58 -0300

    feat: new hot fixes

--- src/migrations/1701704678653-InittialMigration.ts
+++ src/migrations/1701704678653-InittialMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InittialMigration1703340488208 implements MigrationInterface {
-    name = 'InittialMigration1703340488208'
+export class InittialMigration1701704678653 implements MigrationInterface {
+    name = 'InittialMigration1701704678653'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "full_name" character varying(255) NOT NULL, "email" character varying(255) NOT NULL, "password" character varying(255) NOT NULL, "phone_number" character varying(20) NOT NULL, "registration_date" date NOT NULL DEFAULT now(), "delete_date" date, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
